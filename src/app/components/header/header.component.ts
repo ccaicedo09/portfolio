588977a8d5b4e941981827cd1ae42c4da6a4cf08
ccaicedo09 <u20231213568@usco.edu.co>
2025-01-29 15:34:25 -0500

    hero ready

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { NgClass } from '@angular/common';
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +7,26 @@ import { Component, HostListener } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
   hasScrolled = false;
+  activeSection = 'contact';
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    this.hasScrolled = window.scrollY > 50;
+    this.hasScrolled = window.scrollY > 25;
+  }
+
+  ngOnInit() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.activeSection = entry.target.id;
+        }
+      });
+    }, { threshold: 0.5 });
+
+    document.querySelectorAll('section[id]').forEach((section) => {
+      observer.observe(section);
+    });
   }
 }
